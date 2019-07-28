e9a31c28cd491dd151f8af24b6cfb69bc5b50790
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-07-28 02:11:49 -0300

    Update index.js

--- graphql/types/Location/index.js
+++ graphql/types/Location/index.js
@@ -1,16 +1,17 @@
 export default `
   type Location {
     _id: ID!
+    codigo: String!
     cidade: String!
     estado: State!
     location: Position!
   }
   type State {
-    uf: String!,
-    nome: [Float]!
+    uf: String!
+    name: String!
   }
   type Position {
-    type: String!,
+    type: String!
     coordinates: [Float]!
   }
 `;
