be28a20aff86debe6d024b66edc0ac796fd3a961
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-08-17 05:18:02 -0300

    remove unnecessary libs

--- config/tests/rtl.setup.js
+++ config/tests/rtl.setup.js
@@ -1,3 +1,1 @@
-// See https://github.com/kentcdodds/react-testing-library#global-config
-import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
+import "jest-dom/extend-expect";
